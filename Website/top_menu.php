6e3eb7e0c71809c50d7d47d594186b08a49cff94
eelmand <davideelman@gmail.com>
2017-05-31 19:21:04 -0400

    Testing templates

---
+++ Website/top_menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>                        
+			</button>
+			<a class="navbar-brand" href="#">
+				<img alt="Logo" src="images/logo.png" width=20>
+			</a>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="index.html">Home</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="project_plan.html">Project</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Logbooks <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="stan_rashevskyi_log_book.html">Stan Rashevskyi</a></li>
+						<li><a href="david_eelman_log_book.html">David Eelman</a></li>
+						<li><a href="thomas_abdallah_log_book.html">Thomas Abdallah</a></li>
+					</ul>
+				</li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
