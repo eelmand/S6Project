67982fcb18c91822eb9f7c8dcd136e490fc55a7a
eelmand <davideelman@gmail.com>
2017-06-07 13:59:01 -0400

    Playing with javascript controls

--- Website/js/dashboard.js
+++ Website/js/dashboard.js
@@ -1,17 +1,26 @@
-function loopDown(){
-	$("#elevatorPic").animate({marginTop:100}, 1500, function() {
-		$("#example1").sevenSeg({ value: 1 });
-		document.getElementById("elevatorPic").src="images/elevator_2.png";
-		loopUp();
-	});
-}
-
-function loopUp(){
+function floor_3(){
 	$("#elevatorPic").animate({marginTop : 0}, 1500, function() {
-		$("#example1").sevenSeg({ value: 0 });
-		document.getElementById("elevatorPic").src="images/doors_closed.jpg";
-		loopDown();
+		$("#floor-display").sevenSeg({ value: 3 });
+		//document.getElementById("elevatorPic").src="images/doors_closed.jpg";
 	});
 }
 
-loopDown();
+function floor_2(){
+	$("#elevatorPic").animate({marginTop:100}, 1500, function() {
+		$("#floor-display").sevenSeg({ value: 2 });
+	});
+}
+
+function floor_1(){
+	$("#elevatorPic").animate({marginTop:200}, 1500, function() {
+		$("#floor-display").sevenSeg({ value: 1 });
+	});
+}
+
+function open_doors(){
+	document.getElementById("elevatorPic").src="images/elevator_2.png";
+}
+
+function close_doors(){
+	document.getElementById("elevatorPic").src="images/doors_closed.jpg";
+}
