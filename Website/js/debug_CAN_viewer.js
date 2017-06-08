89480bbd29a1efdc8579ea8a6a89986628e352b0
tabdallah <thomas.abdallah@gmail.com>
2017-06-08 11:28:24 -0400

    Adding Javascript to show what the Debug CAN Viewer will look like

---
+++ Website/js/debug_CAN_viewer.js
@@ -0,0 +1,24 @@
+﻿// https://stackoverflow.com/questions/5835126/javascript-infinite-loop
+
+function updateSignal(signal) {
+    var raw_value = 0;
+    var phys_value = ["In Transit", "Floor 1", "Floor 2", "Floor 3"];
+
+    window.setInterval(function() {
+        // Increment raw value from 0 - 3
+        raw_value = (raw_value + 1) & 3;
+
+        // Update raw value in HTML
+        var raw = document.getElementById(signal.concat("_RAW"));
+        raw.innerHTML = raw_value;
+
+        // Update physical value in HTML
+        var phys = document.getElementById(signal.concat("_PHYS"));
+        phys.innerHTML = phys_value[raw_value];
+
+    }, 1000);   // Repeat forever, polling every 3 seconds
+}
+
+window.onload = function start(){
+    updateSignal("EC_Car_Pos");
+}
