21832a3e279adc1a5e7b2d1aea048b463cfe4732
eelmand <davideelman@gmail.com>
2017-06-19 13:58:01 -0400

    Added a chart to dashboard page and updated log book

--- Website/js/dashboard.js
+++ Website/js/dashboard.js
@@ -17,3 +17,66 @@ function floor_1(){
 	});
 }
 
+
+Chart.defaults.global.defaultFontColor = 'white';
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3, 8],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(150, 250, 5, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(150, 250, 5, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Elevator Floors Travelled by Day',
+            fontSize: 18
+        },
+        legend: {
+        	display: false
+        },
+        scales: {
+            yAxes: [{
+            	scaleLabel: {
+        			display: true,
+        			labelString: 'Floors Travelled'
+      			},
+            	gridLines:{
+     				color:"rgba(255,255,255,0.5)",
+      				zeroLineColor:"rgba(255,255,255,0.5)"
+    			},
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            xAxes: [{
+            	scaleLabel: {
+        			display: true,
+        			labelString: 'Days'
+      			},
+            }]
+        }
+    }
+});
