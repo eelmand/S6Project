4c752e9c80d1adbd5865ebd3b0236cbb8c4aba3a
eelmand <davideelman@gmail.com>
2017-06-21 10:05:44 -0400

    Added my age to my log page

--- Website/js/common.js
+++ Website/js/common.js
@@ -1,7 +1,15 @@
 /* Common Javascript code for each webpage */
 
+// General Info
 var today = new Date();
 var year = today.getFullYear();
+
+// Code for showing David's birthday on his log page
+var davidBirthdate = new Date('April 6, 1993 00:00:00');
+var davidAge = today.getTime() - davidBirthdate.getTime();
+davidAge = Math.floor(davidAge / 31556900000)
+var element = document.getElementById('david_age');
+element.innerHTML = '<p>My age is: ' + davidAge + ' years </p>';
 
 // Updates the footer with the current year
 var footer = document.getElementsByTagName("footer");
