d554c29fdb831fe65eecdacc81b3892bcd1c9ebb
tabdallah <thomas.abdallah@gmail.com>
2017-07-25 11:45:57 -0400

    Should work for granting access

--- Website/php/user_management.php
+++ Website/php/user_management.php
@@ -11,10 +11,7 @@
 		$rows = $database->query("SELECT username, password, first_name, last_name, email, last_login FROM new_users WHERE username='" .
 				$username . "' LIMIT 1");
 
-		echo $db->num_rows();
-
-
-		if($rows != FALSE) {
+		if($rows != $username) {
 			foreach($rows as $row){
 				$username = $row['username'];
 				$password = $row['password'];
