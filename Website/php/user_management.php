c111460132e19cbf3c3fcb4d0e833549ded1c191
tabdallah <thomas.abdallah@gmail.com>
2017-07-25 11:33:54 -0400

    debugging grant access

--- Website/php/user_management.php
+++ Website/php/user_management.php
@@ -11,6 +11,9 @@
 		$rows = $database->query("SELECT username, password, first_name, last_name, email, last_login FROM new_users WHERE username='" .
 				$username . "' LIMIT 1");
 
+		var_dump($rows);
+		sleep(10);
+
 		if($rows != FALSE) {
 			foreach($rows as $row){
 				$username = $row['username'];
