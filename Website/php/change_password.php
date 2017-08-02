d1a76fc7032d5d383f810cfc078cc7759b223d96
tabdallah <thomas.abdallah@gmail.com>
2017-08-02 17:39:36 -0400

    a

--- Website/php/change_password.php
+++ Website/php/change_password.php
@@ -1,18 +1,17 @@
 <?php
- {
 	session_start();
 	$username = $_SESSION["username"];
 	$password = $_POST["password"];
 
 	include "update_users.php";
 
-	/*
+	
 	try  {
 		update_password($username, $password);	
 	}
 	catch(Exception $e) {
 		$e->getMessage();
-	}*/
+	}
 
 	header("Location: ../dashboard.html"); /* Redirect browser */
 ?>
