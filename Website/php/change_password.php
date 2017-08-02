49a13b81f93c434bf6dcca552b3e289a4890bd67
tabdallah <thomas.abdallah@gmail.com>
2017-08-02 17:47:20 -0400

    a

--- Website/php/change_password.php
+++ Website/php/change_password.php
@@ -3,6 +3,7 @@
 	$username = $_SESSION["username"];
 	$password = $_POST["password"];
 
+	include "connect_db.php";		// Connect to the remote database		
 	$myquery = "UPDATE users SET password='" . $password . "' WHERE username='" . $username . "'";
 	$query_result = $database->query($myquery);
 
